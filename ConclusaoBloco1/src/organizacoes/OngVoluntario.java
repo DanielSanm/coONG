b9e54b3a6c2ab56c5d085443c652d74b9d12a4ac
DanielSanm <danielelias3221@gmail.com>
2021-09-12 22:33:28 -0300

    Codigo seleção doação usuário

--- ConclusaoBloco1/src/organizacoes/OngVoluntario.java
+++ ConclusaoBloco1/src/organizacoes/OngVoluntario.java
@@ -1,5 +1,49 @@
 package organizacoes;
 
-public class OngVoluntario {
+public class OngVoluntario extends Organizacao {
+	
+	public OngVoluntario() {}
+	
+	public OngVoluntario(String nome, String regiao, String contato, int pix, String necessidades) {
+		this.setNome(nome);
+		this.setRegiao(regiao);
+		this.setContato(contato);
+		this.setPix(pix);
+		
+	}
+	
+	@Override
+	public String nome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public String regiao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String contato() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int pix() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String necessidades() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void mostraOng(Organizacao organizacao) {
+		
+	}
 }
--- ConclusaoBloco1/src/organizacoes/TestaOng.java
+++ ConclusaoBloco1/src/organizacoes/TestaOng.java
@@ -1,5 +1,9 @@
 package organizacoes;
 
 public class TestaOng {
-
+	
+	public void tipoOng(Organizacao ong) {
+		ong.mostraOng(ong);
+	}
+	
 }
